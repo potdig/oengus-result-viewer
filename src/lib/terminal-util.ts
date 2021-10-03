5ce8b1659a6a46100120a138b7c41bfe333be45d
potdig <potadig.ashnerd.zino@gmail.com>
2021-10-04 00:24:53 +0900

    paginate menu

--- src/lib/terminal-util.ts
+++ src/lib/terminal-util.ts
@@ -15,16 +15,41 @@ function viewResults(results: Array<Result>) {
 }
 
 function viewRunsAsMenu(runs: Array<Run>) {
+  const pageSize = 10
+  const pages = runs.reduce(
+    (acc, val, index) =>
+      index % pageSize ? acc : [...acc, runs.slice(index, index + pageSize)],
+    [] as Array<Array<Run>>
+  )
+
+  createMenu(pages, 0)
+}
+
+function createMenu(pages: Array<Array<Run>>, index: number) {
+  const page = pages[index]
+
   const menu = new TerminalMenu({
+    width: 120,
     bg: 'black'
   })
-  menu.writeLine('Submissions')
+  menu.writeLine(`Submissions ${index + 1} / ${pages.length}`)
   menu.writeSeparator()
-  runs.forEach((run) => {
+  page.forEach((run) => {
     menu.add(run.formatted, () => {
       console.log(`Selected ID: ${run.id}`)
     })
   })
+  if (index > 0) {
+    menu.add(' << BACK', () => {
+      createMenu(pages, index - 1)
+    })
+  }
+  if (index < pages.length - 1) {
+    menu.add(' NEXT >>', () => {
+      createMenu(pages, index + 1)
+    })
+  }
+  return menu
 }
 
 export { viewRuns, viewResults, viewRunsAsMenu }
